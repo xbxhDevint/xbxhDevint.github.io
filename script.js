da7a740aa474e86a5ce9ad26f435e7fc31ab1330
Devint <U23ayo26@uchon.es.kr>
2024-12-20 10:44:28 +0900

    update script.js

--- script.js
+++ script.js
@@ -3,10 +3,10 @@ fetch('https://www.younghoon.ms.kr/api/school_food_api.jsp').then(res=>{return r
   const date = new Date()
   meal.forEach(function(meal) {
     if (meal["MLSV_YMD"]==`${date.getFullYear()}${(date.getMonth()+1).toString().padStart(2,"0")}${date.getDate().toString().padStart(2,"0")}`) {
-      document.getElementById("b").innerHTML = meal["DDISH_NM"];//.replaceAll(".", "").replaceAll("1", "").replaceAll("2", "").replaceAll("3", "").replaceAll("4", "").replaceAll("5", "").replaceAll("6", "").replaceAll("7", "").replaceAll("8", "").replaceAll("9", "").replaceAll("0", "").replaceAll("(","").replaceAll(")","");
+      document.getElementById("b").innerHTML = meal["DDISH_NM"];
     }
   });
   if (document.getElementById("b").innerHTML == "") {
     document.getElementById("b").innerHTML = "오늘 급식없다 멍충아";
   }
-})
+});
