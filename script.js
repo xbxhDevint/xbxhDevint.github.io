29227d349b099aee651e2f869eea9da0f317dfc8
Devint <U23ayo26@uchon.es.kr>
2024-12-26 07:48:43 +0900

    Update script.js

--- script.js
+++ script.js
@@ -1,15 +1,15 @@
-fetch('https://www.younghoon.ms.kr/api/school_food_api.jsp').then(res=>{return res.json()}).then(json=>{
+const date = new Date()
+const hour = date.getHours().toString().padStart(2, "0")
+fetch('https://www.younghoon.ms.kr/api/school_food_api.jsp').then(res => { return res.json() }).then(json => {
   const meal = json["result"]
-  const date = new Date()
   meal.forEach(function(meal) {
-    if (meal["MLSV_YMD"]==`${date.getFullYear()}${(date.getMonth()+1).toString().padStart(2,"0")}${date.getDate().toString().padStart(2,"0")}`) {
-      document.getElementById("b").innerHTML = meal["DDISH_NM"];
+    if (meal["MLSV_YMD"] == `${date.getFullYear()}${(date.getMonth() + 1).toString().padStart(2, "0")}${date.getDate().toString().padStart(2, "0")}`) {
+      document.getElementById("meal").innerHTML = meal["DDISH_NM"];
     }
   });
-  if (document.getElementById("b").innerHTML == "") {
-    document.getElementById("b").innerHTML = "오늘 급식없다 멍충아";
+  if (document.getElementById("meal").innerHTML == "") {
+    document.getElementById("meal").innerHTML = "오늘 급식은 없습니다";
   }
 });
-document.querySelector('#bg').addEventListener('click', () => {
-  document.querySelector('#bg').src = 'night.png';
-})
+document.getElementById('date').innerHTML = `${date.getMonth()+1}/${date.getDate()} ${date.toString().split(" ")[0]}`;
+document.getElementById('time').innerHTML = `${hour >12 ? (hour -12).toString().padStart(2,"0") :hour }:${date.getMinutes().toString().padStart(2, "0")}`;
